4e8339872a7b83403d395d20b6a6f7d82300106c
BunducPaul <117160428+BunducPaul@users.noreply.github.com>
2022-11-17 17:32:12 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,93 @@
+function onlyLetters() {
+  if (/^[a-zA-Z]+$/.test(document.getElementById("input").value) == true) {
+    addWordhidden(), addletters();
+  } else if (document.getElementById("input").value.length == 0) {
+    document.getElementById("advertisementOrhiddenWord").innerHTML = "empty";
+  } else {
+    document.getElementById("advertisementOrhiddenWord").innerHTML = "only Letters";
+  }
+}
+
+function addWordhidden() {
+  document.getElementById("advertisementOrhiddenWord").innerHTML = "";
+  for (let i = 0; i < document.getElementById("input").value.length; ++i) {
+    let hiddenLetter = document.createElement("text");
+    Object.assign(hiddenLetter, {
+      id: i,
+      textContent: "_ ",
+    });
+    document
+      .getElementById("advertisementOrhiddenWord")
+      .appendChild(hiddenLetter);
+  }
+}
+
+function addletters() {
+  document.getElementById("inputAndbutton").style = "display:none";
+  let allLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  for (let j = 0; j <= 25; ++j) {
+    let button = document.createElement("button");
+    Object.assign(button, {
+      id: allLetters.charAt(j),
+      textContent: allLetters.charAt(j),
+      onclick: () => checkThechosenLetter(button.id),
+    });
+    document.getElementById("btnGroupalphabet").appendChild(button);
+  }
+}
+
+let buttonPressed = 0;
+
+function checkThechosenLetter(id) {
+  ++buttonPressed;
+  let word = document.getElementById("input").value.toUpperCase();
+  let hiddenLettersleft = 0;
+  let chosenLetter = 0;
+  for (let i = 0; i < word.length; ++i) {
+    if (id == word.charAt(i)) {
+      for (let j = 0; j < word.length; ++j) {
+        if (word.charAt(j) == id) {
+          ++chosenLetter;
+          document.getElementById([j]).textContent = id;
+          document.getElementById([id]).disabled = true;
+        }
+      }
+    } else {
+      document.getElementById([id]).disabled = true;
+    }
+    if (document.getElementById([i]).textContent == "_ ") {
+      ++hiddenLettersleft;
+    }
+  }
+  if (chosenLetter > 0) {
+    --buttonPressed;
+    chosenLetter = 0;
+  }
+  if (buttonPressed >= 6) {
+    restartGame("YOU LOST THE GAME :(");
+  }
+  if (buttonPressed > 0 && buttonPressed <= 5) {
+    document.getElementById("img").src ="img/" + buttonPressed + ".png";
+  }
+  if (hiddenLettersleft == 0) {
+    restartGame("YOU WON THE GAME :)");
+  }
+}
+
+function startGame() {
+  document.getElementById("inputAndbutton").style = "display:block";
+  document.getElementById("endOftheGame").style = "display:none";
+  document.getElementById("startScreen").style = "display:none";
+  document.getElementById("gamePage").style = "display:block";
+}
+
+function restartGame(mesaj) {
+  buttonPressed = 0;
+  document.getElementById("img").src ="img/0.png";
+  document.getElementById("btnGroupalphabet").innerHTML = "";
+  document.getElementById("advertisementOrhiddenWord").innerHTML = "";
+  document.getElementById("input").innerHTML = "";
+  document.getElementById("gamePage").style = "display:none";
+  document.getElementById("endOftheGame").style = "display:block";
+  document.getElementById("finalMessage").innerHTML ='\n'+ mesaj;
+}
